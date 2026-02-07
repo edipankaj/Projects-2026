ed273eaf9b24f373ab769e0ff2891b7d4081ee27
Pankaj Kumar <edi.bainspankajkumar@gmail.com>
2026-02-07 16:55:45 +0000

    2

---
+++ c++/day-1/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main () {
+    int x; // dec
+    int y;
+    cin >> y;
+    cin >> x;
+    cout << "Addition: " << endl;
+    cout <<"subtract "  x-y << endl;
+    cout <<"Multiply "  x*y << endl;
+    cout <<"division "  x/y << endl;    
+    return 0;
+}
