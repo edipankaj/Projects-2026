02ab50b3ce5afb8203b092dc883a5848979d7699
Pankaj Kumar <edi.bainspankajkumar@gmail.com>
2026-02-07 18:04:21 +0000

    identifiers

---
+++ uses of identifiers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main () {cout << "What's your name? " << endl;
+    char NAME[100];
+    cout << " Your name is: jj " << NAME << endl;
+    cout << "what is Your age ? " << endl;
+    int age;
+    cin >> NAME;
+    return 0;   
+    cin >> age;
+
+    cout << "Your age is: " << age << endl;
+}
